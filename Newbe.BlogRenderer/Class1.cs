41a9762e436e7a06e51d2ce21788e6036fed37c5
Newbe36524 <newbe36524@qq.com>
2023-01-24 10:40:54 +0800

    init

---
+++ Newbe.BlogRenderer/Class1.cs
@@ -0,0 +1,12 @@
+﻿using Markdig.Helpers;
+using Markdig.Parsers;
+
+namespace Newbe.BlogRenderer;
+
+public class Class1 : InlineParser
+{
+    public override bool Match(InlineProcessor processor, ref StringSlice slice)
+    {
+        throw new NotImplementedException();
+    }
+}
